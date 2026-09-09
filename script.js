bf16e0e0db730d3f6312c2f0aab70167ee3fe6eb
Nom411 <yohanlopez1412@gmail.com>
2026-09-09 17:13:43 -0500

    semana 4

---
+++ script.js
@@ -0,0 +1,34 @@
+document.getElementById("footer").innerHTML = `
+<footer>
+
+    <div class="footer-logo">
+        THE LIBERTY
+    </div>
+
+    <p>
+        Visita Nuestro Instagram
+    </p>
+
+    <a href="https://www.instagram.com/the.liberty.bar/"
+       target="_blank"
+       class="instagram-footer">
+
+        <img src="img/the liberty insta 2.jpeg"
+             alt="Visita nuestro Instagram">
+
+    </a>
+
+    <div class="footer-bottom">
+
+        <span>
+            © 2026 The Liberty
+        </span>
+
+        <span>
+            Proyecto de aula
+        </span>
+
+    </div>
+
+</footer>
+`;
